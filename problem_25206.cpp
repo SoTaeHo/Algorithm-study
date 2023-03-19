4c144df8e2692b1f8e1783032b80c878fc16c4ef
소태호 <sth9057@gmail.com>
2023-03-19 16:52:05 +0900

    Start

---
+++ problem_25206.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+
+    float num;
+    string subject[20];
+    float credit[20];
+    string score[20];
+
+    for(int i = 0; i < 20; i++) {
+        cin >> subject[i] >> credit[i] >> score[i];
+    }
+    cout << arr[0][1] << endl;
+
+    return 0;
+}
