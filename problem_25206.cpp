8475599a4588a2c9eab650bf419f0e5c6bbabfd0
소태호 <sth9057@gmail.com>
2023-06-24 12:43:13 +0900

    다수의 main 함수를 만들 수 있게 적용
    사용 플러그인 : new executable c/c++
    사용방법 : 플러그인 설치 후 alt + shift + e 를 통해 cmakelists.txt에 add_excutable을 자동으로 삽입 > configuration에 해당 파일의 custom configuration이 자동으로 생성

--- problem_25206.cpp
+++ problem_25206.cpp
@@ -1,57 +1,57 @@
-//#include <iostream>
-//using namespace std;
-//
-//
-//int main(void) {
-//
-//    string subject[20];
-//    float credit[20];
-//    string score[20];
-//    float result;
-//    float cnt;
-//
-//    for(int i = 0; i < 20; i++) {
-//        cin >> subject[i] >> credit[i] >> score[i];
-//    }
-//
-//    for(int i =0; i < 20; i++) {
-//       if (score[i] == "A+") {
-//           result += credit[i] * 4.5;
-//           cnt += credit[i];
-//       } else if(score[i] == "A0") {
-//           result += credit[i] * 4.0;
-//           cnt += credit[i];
-//       } else if(score[i] == "B+") {
-//           result += credit[i] * 3.5;
-//           cnt += credit[i];
-//
-//       } else if(score[i] == "B0") {
-//           result += credit[i] * 3.0;
-//           cnt += credit[i];
-//
-//       } else if(score[i] == "C+") {
-//           result += credit[i] * 2.5;
-//           cnt += credit[i];
-//
-//       } else if(score[i] == "C0") {
-//           result += credit[i] * 2.0;
-//           cnt += credit[i];
-//
-//       } else if(score[i] == "D+") {
-//           result += credit[i] * 1.5;
-//           cnt += credit[i];
-//
-//       } else if(score[i] == "D0") {
-//           result += credit[i] * 1.0;
-//           cnt += credit[i];
-//
-//       } else if(score[i] == "F") {
-//           result += credit[i] * 0.0;
-//           cnt += credit[i];
-//       }
-//
-//    }
-//    cout << result / cnt << endl;
-//
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+
+    string subject[20];
+    float credit[20];
+    string score[20];
+    float result;
+    float cnt;
+
+    for(int i = 0; i < 20; i++) {
+        cin >> subject[i] >> credit[i] >> score[i];
+    }
+
+    for(int i =0; i < 20; i++) {
+       if (score[i] == "A+") {
+           result += credit[i] * 4.5;
+           cnt += credit[i];
+       } else if(score[i] == "A0") {
+           result += credit[i] * 4.0;
+           cnt += credit[i];
+       } else if(score[i] == "B+") {
+           result += credit[i] * 3.5;
+           cnt += credit[i];
+
+       } else if(score[i] == "B0") {
+           result += credit[i] * 3.0;
+           cnt += credit[i];
+
+       } else if(score[i] == "C+") {
+           result += credit[i] * 2.5;
+           cnt += credit[i];
+
+       } else if(score[i] == "C0") {
+           result += credit[i] * 2.0;
+           cnt += credit[i];
+
+       } else if(score[i] == "D+") {
+           result += credit[i] * 1.5;
+           cnt += credit[i];
+
+       } else if(score[i] == "D0") {
+           result += credit[i] * 1.0;
+           cnt += credit[i];
+
+       } else if(score[i] == "F") {
+           result += credit[i] * 0.0;
+           cnt += credit[i];
+       }
+
+    }
+    cout << result / cnt << endl;
+
+    return 0;
+}
