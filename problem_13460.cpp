5dcd088725fee918ba905d229883bc10dd4d9537
소태호 <sth9057@gmail.com>
2023-09-15 19:19:51 +0900

    problem 13460 solved
    1. BFS에서 탐색을 하는 중 현재 위치가 시작점으로부터 얼마나 떨어져있는지를 구하는 과정을 생각하던 중 착오가 있었음
    BFS는 너비 우선 탐색 -> 현재 위치에서 갈 수 있는 모든 위치를 탐색한다. 최단 경로를 구해야 할 때 어떤 위치를 방문했을 때 바로 방문처리를 한다. 어떤 경로로 접근해도 이미 방문처리가 되어있다면 현재 경로로 접근하는 방법보다 더 빠르게 해당 위치로 도착할 수 있는 경우의 수가 있다는 얘기.
    해당 문제의 경우 처음 R의 위치만을 가지고 방문 처리를 하여 경로 중 B의 위치와 상관없이 R이 한 번이라도 거쳤던 곳에 다시 가게되면 탐색이 제대로 진행되지 않았었다. N * M의 정수형 배열에 한 번 움직일 때마다 +1을 하여 O와 만났을 때 해당 배열의 값을 리턴하게 시도했지만 실패했다.
    방문 조건을 R과 B 두 가지 모두 확인하는 것이 떠올라, set, vector등으로 구현해보려 하였지만 R과 B의 위치가 제대로 저장되지 않고, 이미 방문한 위치인지 확인하는 과정이 번거로워 검색을 통해 4차원 배열 visited를 선언하여 해결하였다.
    아직 전역변수나 고정된 길이의 배열을 사용하는 것이 어색하여 생각하지 못한 것 같다.

--- problem_13460.cpp
+++ problem_13460.cpp
@@ -5,58 +5,61 @@ using namespace std;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
-queue<pair<int, int>> r_q;
-queue<pair<int, int>> b_q;
-vector<vector<int>> visited;
+queue<vector<int>> q;
+bool visited[10][10][10][10];
 vector<vector<char>> map;
 
-pair<int, int> r;
-pair<int, int> b;
-pair<int, int> o;
-
 void bfs() {
-    cout << "bfs start\n";
     int nx_r;
     int ny_r;
     int nx_b;
     int ny_b;
-
-    // back traking
-    while(!r_q.empty()) {
-        r = r_q.front();
-        r_q.pop();
-
-        b = b_q.front();
-        b_q.pop();
+    int cnt;
+    while(!q.empty()) {
+        vector<int> vec = q.front();
+        q.pop();
 
         for (int i = 0; i < 4; i++) {
-            nx_r = r.first;
-            ny_r = r.second;
-            nx_b = b.first;
-            ny_b = b.second;
+            nx_r = vec[0];
+            ny_r = vec[1];
+            nx_b = vec[2];
+            ny_b = vec[3];
+            cnt = vec[4] + 1;
             int distance = 0;
-            // 가려는 방향이 바로 벽으로 막히면 다음 방향으로 continue
-            if (visited[nx_r + dx[i]][ny_r + dy[i]] == -1) {
-                continue;
+
+            bool b_out = false;
+            if (cnt > 10) {
+                cout << -1;
+                return;
             }
-            while (map[nx_b + dx[i]][ny_r + dy[i]] != '#') {
+            while (map[nx_b + dx[i]][ny_b + dy[i]] != '#') {
+
                 nx_b += dx[i];
                 ny_b += dy[i];
                 distance++;
                 if (map[nx_b][ny_b] == 'O') {
-                    cout << -1;
-                    return;
+                    b_out = true;
                 }
             }
+
+            if (b_out)
+                continue;
+
             while (map[nx_r + dx[i]][ny_r + dy[i]] != '#') {
                 nx_r += dx[i];
                 ny_r += dy[i];
                 distance--;
                 if (map[nx_r][ny_r] == 'O') {
-                    cout << visited[r.first][r.second] + 1;
-                    return;
+                    if (cnt <= 10) {
+                        cout << cnt;
+                        return;
+                    } else {
+                        cout << -1;
+                        return;
+                    }
                 }
             }
+
             // 동일한 행 또는 동일한 열에서 이동할 경우 움직인 거리에 따라 많이 움직인 구슬의 위치를 조정해준다
             if (nx_r == nx_b && ny_r == ny_b) {
                 if (distance > 0) {
@@ -69,14 +72,23 @@ void bfs() {
                 }
             }
 
+            // 이미 방문했던 경로에 다시 방문을 해야하는 해의 경우 어떻게 처리를 해야할까
+            // - 현재는 이미 방문했던 곳은 현재 진행한 횟수보다 많은 횟수로 도달하면 방문처리를 하지 않음
+            // 이미 존재했던 R과 B의 위치 >> 다시 돌아올 수 있는 곳 >> 큐에 집어넣지 않음
 
-            if (visited[nx_r][ny_r] > visited[r.first][r.second] + 1 || visited[nx_r][ny_r] == 0) {
-                visited[nx_r][ny_r] = visited[r.first][r.second] + 1;
+            if (!visited[nx_r][ny_r][nx_b][ny_b]) {
                 if (map[nx_r][ny_r] == 'O') {
-                    cout << visited[nx_r][ny_r];
-                    return;
+                    if (cnt <= 10) {
+                        cout << cnt;
+                        return;
+                    }
+                    else {
+                        cout << -1;
+                        return;
+                    }
                 }
-                r_q.emplace(nx_r, ny_r);
+                visited[nx_r][ny_r][nx_b][ny_b] = true;
+                q.push({nx_r, ny_r, nx_b, ny_b, cnt});
             }
         }
     }
@@ -88,38 +100,33 @@ int main (void) {
 
     cin >> n >> m;
 
-    visited.resize(n , vector<int>(m, 0));
     map.resize(n, vector<char>(m, 0));
-
+    int rx, ry, bx, by;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> map[i][j];
-            if (map[i][j] == '#')
-                visited[i][j] = -1;
-            else if (map[i][j] == 'R') {
-                r = make_pair(i, j);
-                r_q.push(r);
+            if (map[i][j] == 'R') {
+                rx = i;
+                ry = j;
             }
             else if (map[i][j] == 'B') {
-                b = make_pair(i, j);
-                b_q.push(b);
+                bx = i;
+                by = j;
             }
-            else if (map[i][j] == 'O')
-                o = make_pair(i, j);
         }
     }
-
+    visited[rx][ry][bx][by] = true;
+    q.push({rx, ry, bx, by, 0});
 //    1. bfs를 실행할 때 갈 수 있는 만큼 이동
     bfs();
 //    2. O를 만나면 종료 / 두 구슬이 같은 칸에 있을 수 없음
 //    3. -1이 되는 경우 : R이 O에 도달할 수 없을 때 / R과 B가 동시에 O에 도달할 때 / B가 먼저 O에 도달할 때
 
-    // print vector
-    for (auto & ptr : map) {
-        for (auto & p : ptr) {
-            cout << p;
-        }
-        cout << '\n';
-    }
-
+//    // print vector
+//    for (auto & ptr : map) {
+//        for (auto & p : ptr) {
+//            cout << p;
+//        }
+//        cout << '\n';
+//    }
 }
