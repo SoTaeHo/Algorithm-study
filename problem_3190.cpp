de963a5f19bd9d322dde4657b191bd0d79c4c825
소태호 <sth9057@gmail.com>
2023-09-26 19:00:40 +0900

    problem_3190 solved
    앞선 문제들보다 쉬운 편이었고, 새로운 자료구조 deque를 사용해볼 수 있는 기회였다.
    
    1. 실수한 부분
    특정 초 마다 뱀의 머리를 회전시켜줘야해서 dx, dy배열을 90도씩 바꿀 수 있게 상, 우, 하, 좌 순서로 방향을 설정해놓고 dir 변수를 통해 dx의 방향을 바꿔주게 하였다. 이 때 L이 들어올 때 왼쪽으로 90도를 꺾어야 한다. 즉 dx[dir]과 dy[dir]의 dir이 감소해야한다고 생각하여 (dir - 1) % 4 라고하여 dir이 0인 경우 제대로 작동하지 않았다.
    뱀이 사과를 먹었다면 해당 부분의 map 배열의 값을 0으로 바꿔주지 않아 사과를 중복해서 먹을 가능성이 있었다.

--- problem_3190.cpp
+++ problem_3190.cpp
@@ -1,10 +1,97 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 typedef vector<vector<int>> dv;
 
+int n;
+int k;
+int l;
+int x = 0;
+
+// right, down, left, up
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+int dir = 0;
+deque<pair<int, int>> body;
+
+vector<pair<int, char>> direction;
+dv map;
+
+bool is_valid(int nx, int ny) {
+
+    for (auto & ptr : body) {
+        if (ptr.first == nx && ptr.second == ny) {
+            return false;
+        }
+    }
+    return (nx >= 0 && ny >= 0 && nx < n && ny < n);
+}
+
 int main(void) {
 
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+    cin >> n >> k;
+
+    map.resize(n, vector<int>(n, 0));
+    for (int i = 0; i < k; i++) {
+        int a, b;
+        cin >> a >> b;
+        map[a - 1][b - 1] = 1; // 1 means there is apple;
+    }
+
+    cin >> l;
+
+    for (int i = 0; i < l; i++) {
+        int a;
+        char b;
+        cin >> a >> b;
+        direction.emplace_back(a, b);
+    }
+
+    body.emplace_front(0, 0);
+    int cnt = 0;
+    while (1) {
+
+        int nx = body.front().first;
+        int ny = body.front().second;
+
+        if (direction[cnt].first == x) {
+
+            if (direction[cnt].second == 'D') { // turn right;
+                dir = (dir + 1) % 4;
+            } else if (direction[cnt].second == 'L') { // turn left;
+                dir = (dir + 3) % 4; // mistake point
+            }
+            cnt++;
+        }
+
+        if (!is_valid(nx + dx[dir], ny + dy[dir])) {
+            x++;
+            break;
+        }
+
+        body.emplace_front(nx + dx[dir], ny + dy[dir]);
+        if(map[nx + dx[dir]][ny + dy[dir]] != 1) { // if there is no apple
+            body.pop_back();
+        } else {
+            map[nx + dx[dir]][ny + dy[dir]] = 0; // mistake point
+        }
+        x++;
+    }
+
+    cout << x;
+    cout << (0 - 1) % 4;
+//    cout << x << '\n';
+//    cout << body.size() << '\n';
+//
+//    for(auto & ptr : map) {
+//        for (auto & p : ptr) {
+//            cout << p << ' ';
+//        }
+//        cout << '\n';
+//    }
 }
