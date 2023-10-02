e4eef8131d4784636c2181327e63f891f7fdf0fd
소태호 <sth9057@gmail.com>
2023-10-03 00:37:19 +0900

    problem 13458 solved
    N이 1 ~ 1,000,000 이며 A[i] 또한 1 ~ 1,000,000이라 모든 감독관의 수를 더해야하는 result 변수는 최대 1,000,000 * 1,000,000 (B = 1, C = 1)를 담을 수 있는 long long 자료형으로 선언해주지 않아 fail을 받았다.
    
    또한 부감독관의 수를 구하는 과정을 나눗셈과 모듈러를 사용하지 않고 반복문을 중첩해서 사용하여 시간 초과를 받았다.
    
    한 시험장의 인원보다 총감독관의 감독가능 인원이 더 큰 경우를 고려하지 않아 34번째 줄에서 candidate == 0이라고 작성하여 fail을 받았다.
    
    거기에 더해 해당 조건문에서 continue가 아닌 break를 사용하여 fail을 받았다.
    
    응당 생각해봐야 했던 점들을 모두 간과하고 수학적 원리만 적용하려했고, 문제에서 제시한 조건을 충족하는지 꼼꼼하게 확인하지 않았다. 반성을 많이 하게 되는 문제

--- problem_13458.cpp
+++ problem_13458.cpp
@@ -1,14 +1,44 @@
 #include "iostream"
-
+#include "vector"
 using namespace std;
 
 int n, b, c;
+long long result = 0;
+vector<int> a;
 
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     cin >> n;
+    a.resize(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cin >> b >> c;
 
-    for (int i = 0; )
+//    time out
+//    for (int i = 0; i < n; i++) {
+//        int candidate = a[i];
+//        candidate -= b;
+//        result++;
+//        while (candidate > 0) {
+//            candidate -= c;
+//            result++;
+//        }
+//    }
+    for (int i = 0; i < n; i++) {
+        int candidate = a[i];
+        candidate -= b;
+        result++;
+        if (candidate <= 0) {
+            continue;
+        }
+        if (candidate % c == 0) {
+            result += candidate / c;
+        } else {
+            result += (candidate / c) + 1;
+        }
+    }
+    cout << result;
 }
