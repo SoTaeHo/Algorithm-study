9d92229ef3709eadd35ab9136f10aaa5e8b1ad2d
소태호 <sth9057@gmail.com>
2023-10-05 14:00:24 +0900

    problem 14499 try 1

--- problem_14499.cpp
+++ problem_14499.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 typedef vector<vector<int>> dv;
 struct dice {
-    int top = 0;
-    int bottom = 0;
     int front = 0;
     int behind = 0;
-    int right = 0;
     int left = 0;
+    int bottom = 0;
+    int right = 0;
+    int top = 0;
 };
 int n, m, x, y, k;
 dv map;
@@ -30,13 +30,13 @@ int main(void) {
 
     for (int i = 0; i < k; i++) {
         cin >> k;
-        if (k == 1) {
+        if (k == 1) { // east : left > bottom, bottom > right, right > top, top > left
 
-        } else if (k == 2) {
+        } else if (k == 2) { // west : reverse of east
 
-        } else if (k == 3) {
+        } else if (k == 3) { // north : behind > bottom, bottom > front, front > top, top > behind
 
-        } else if (k == 4) {
+        } else if (k == 4) { // south : reverse of north
 
         }
     }
