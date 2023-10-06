33281fe0b2ae6751e4ea53b13785c13b331dfee3
소태호 <sth9057@gmail.com>
2023-10-06 14:37:11 +0900

    problem 14499 solved
    주사위의 6개 면을 front, behind, top, bottom, left, right으로 두고, 주사위가 어떤 방향으로 이동할 때 이동 후 6개의 면의 위치가 어떻게 바뀌는지만 생각해내면 풀 수 있는 문제
    
    문제 푸는 방법을 검색해본 결과 서쪽으로 1번 굴리는 것은 동쪽으로 3번 굴리는 것과 같다는 아이디어를 보았다. 입력으로 받는 dir에 따라 모든 방향으로 1번씩 굴리는 것으로 구현했는데, 면을 바꿔주는 부분을 빼내어 좌우(동서)이동, 상하(남북)이동으로 함수를 만들어 인자로 반복횟수를 주면 조금 더 코드를 줄일 수 있을 것 같다.

--- problem_14499.cpp
+++ problem_14499.cpp
@@ -1,5 +1,6 @@
 #include "iostream"
 #include "vector"
+#include "algorithm"
 using namespace std;
 
 typedef vector<vector<int>> dv;
@@ -13,7 +14,30 @@ struct dice {
 };
 int n, m, x, y, k;
 dv map;
+dice d;
 
+bool movable(int dir) {
+    if (dir == 1) {
+        return (y + 1 < m);
+    } else if (dir == 2) {
+        return (y - 1 >= 0);
+    } else if (dir == 3) {
+        return (x - 1 >= 0);
+    } else if (dir == 4) {
+        return (x + 1 < n);
+    } else
+        return false;
+}
+
+void change_map() {
+    if (map[x][y] == 0) {
+        map[x][y] = d.bottom;
+    } else {
+        d.bottom = map[x][y];
+        map[x][y] = 0;
+    }
+    cout << d.top << '\n';
+}
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -27,17 +51,43 @@ int main(void) {
             cin >> map[i][j];
         }
     }
-
     for (int i = 0; i < k; i++) {
-        cin >> k;
-        if (k == 1) { // east : left > bottom, bottom > right, right > top, top > left
-
-        } else if (k == 2) { // west : reverse of east
-
-        } else if (k == 3) { // north : behind > bottom, bottom > front, front > top, top > behind
-
-        } else if (k == 4) { // south : reverse of north
-
+//        cout << "now x, y :" << x << ", " << y << '\n';
+        int dir;
+        cin >> dir;
+        int temp;
+        if (dir == 1 && movable(dir)) { // east : left > bottom, bottom > right, right > top, top > left
+            y += 1;
+            temp = d.left;
+            d.left = d.bottom;
+            d.bottom = d.right;
+            d.right = d.top;
+            d.top = temp;
+            change_map();
+        } else if (dir == 2 && movable(dir)) { // west : reverse of east
+            y -= 1;
+            temp = d.left;
+            d.left = d.top;
+            d.top = d.right;
+            d.right = d.bottom;
+            d.bottom = temp;
+            change_map();
+        } else if (dir == 3 && movable(dir)) { // north : behind > bottom, bottom > front, front > top, top > behind
+            x -= 1;
+            temp = d.behind;
+            d.behind = d.bottom;
+            d.bottom = d.front;
+            d.front = d.top;
+            d.top = temp;
+            change_map();
+        } else if (dir == 4 && movable(dir)) { // south : reverse of north
+            x += 1;
+            temp = d.behind;
+            d.behind = d.top;
+            d.top = d.front;
+            d.front = d.bottom;
+            d.bottom = temp;
+            change_map();
         }
     }
 }
