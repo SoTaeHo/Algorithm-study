77e385804611bea6d190b5ac481252a383aeff06
소태호 <sth9057@gmail.com>
2023-10-12 20:36:03 +0900

    problem 1753 failed
    1. 우선순위 큐
    최소 힙 구조를 사용하여 큐에서 원소를 1개 pop할 때 가장 큰 값을 선택하여 pop해준다. priority_queue 선언 시 Compare 구조체를 작성해주면 사용자가 원하는 방법으로 정렬시켜 사용할 수 있다.
    현재 노드에서 최단거리에 있는 노드와 연결된 간선을 업데이트 하는 것은 동일하지만, 현재 노드에서 최단거리에 있는 노드를 구할 때 시간 복잡도는 우선순위 큐 : O(logV) / 선형 자료구조 : O(V)이므로, 노드의 수가 늘어날수록 차이는 더 크게 벌어진다.
    
    2. 다익스트라 알고리즘
    다익스트라 알고리즘은 그리디 알고리즘이므로 현재 노드에서 최단 거리에 있는 노드를 선택해나가며 노드를 탐색한다. 선형 구조/힙 구조를 사용해 선택하여 방문한 노드는 최단거리가 증명되므로 다시 방문할 필요가 없어진다.(증명, 즉 A에서 B로 방문할 때 원래 알고 있던 시작점 - B 인 d[B]보다 시작점 - A - B인 d[A] + P[A][B]가 더 작지 않은 이상 d[B]는 항상 최소값 유지하며 큐에 더 이상 넣지 않음)
    
    3. 메모리 초과
    메모리 초과가 난다고 int를 모두 char로 바꾸는 바보짓을 해버려 입력 V가 20000까지 고려되지 못하여 런타임에러(Out of bound)가 발생했었다. 문제를 꼼꼼하게 읽고 입력의 최대, 최소를 고려한 자료형 선정을 해야한다.

--- problem_1753_dijkstra.cpp
+++ problem_1753_dijkstra.cpp
@@ -1,7 +1,59 @@
+#include <algorithm>
 #include "iostream"
-
+#include "queue"
+#include "vector"
 using namespace std;
+//#define INF numeric_limits<int>::max()
+#define INF 3000001
+int v, e, k;
+vector<vector<pair<int, int>>> path;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+vector<int> d;
 
 int main(void) {
+    cin >> v >> e >> k;
 
+    int start, end, weight;
+    path.resize(v);
+    for (int i = 0; i < e; i++) {
+        cin >> start >> end >> weight;
+        path[start - 1].emplace_back(make_pair(weight, end - 1));
+    }
+    d.resize(v, INF);
+    d[k - 1] = 0;
+
+    pq.emplace(0, k - 1);
+    while (!pq.empty()) {
+        int x = pq.top().first;
+        int N = pq.top().second;
+        pq.pop();
+
+        for (int i = 0; i < path[N].size(); i++) {
+            int next_W = path[N][i].first;
+            int next_V = path[N][i].second;
+
+            if (x + next_W < d[next_V]) { // 최소거리가 갱신되면 우선순위 큐에 삽입
+                d[next_V] = x + next_W;
+                pq.emplace(x + next_W, next_V);
+            }
+        }
+
+    }
+    for (int i = 0; i < v; i++) {
+        if (d[i] == INF)
+            cout << "INF" << '\n';
+        else
+            cout << d[i] << '\n';
+    }
 }
+
+// 다익스트라 알고리즘
+// 1. S = 방문한 노드의 집합, D[i] = 임의의 시작점으로부터 D[i]점까지 계산된 최단 거리, Q = 방문하지 않은 노드의 집합
+// 2. 시작점의 D[i] = 0, 나머지 노드의 d[i]는 무한대로 설정
+// 3. int infinity = std::numeric_limits<int>::max() 사용 / 문제를 고려하여 무한대로 간주할 수 있는 수 사용
+// 4. D[i]가 최소인 i를 고름
+// 5. i에서 방문가능한 노드 N에 대해 D[i] + P[i][N] < D[N] ? D[i] + P[i][N] : D[N]
+// 6. i를 S에 넣어 방문처리
+// 7. 미방문 상태인 노드 중 D[i]값이 가장 작은 노드 선택
+// 8. 도착 노드가 방문 완료되거나 Q에 더 이상 노드가 없을 때 까지 5 ~ 7 반복
+// 9. 도착 노드의 D[i]값이 시작점으로부터 최단 거리, INF라면 길이 끊긴 것을 의미
