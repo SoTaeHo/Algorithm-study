fcf36ed4535c913065f35d5c3edf1676cb182f81
소태호 <sth9057@gmail.com>
2024-03-14 16:14:07 +0900

    problem_15684.cpp failed
    문제에서 1부터 시작한다는 얘기가 있다면, 배열을 1부터 시작하게끔 만들어야겠다.
    
    0부터 하려고 떼를 쓰다가 조건식만 더 복잡해졌다.
    
    아직도 사다리 배열에 대한 이해가 안된다.

--- problem_15684.cpp
+++ problem_15684.cpp
@@ -3,10 +3,72 @@
 
 using namespace std;
 
-int N, M, H;
+vector<vector<int>> map;
+//vector<vector<bool>> link;
+bool link[11][30];
+
+int N, M, H, result = 99999999;
+int min(int a, int b) {if (a < b) return a; return b;}
+
+bool check() {
+
+    for (int i = 1; i <= N; i++) {
+        int line = i;
+        for (int j = 1; j <= H; j++) {
+            if (link[line][j]) {
+                line += 1;
+            }
+            else if (link[line - 1][j]) {
+                line -= 1;
+            }
+        }
+        if (line != i) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void loop(int idx, int cnt) {
+    if (cnt >= 4) {
+        return;
+    }
+
+    if(check()) {
+        result = min(result, cnt);
+        return;
+    }
+
+    // H * N - 1의 2차원 벡터
+    for (int i = idx; i <= H; i++) {
+        for (int j = 1; j < N; j++) {
+            if (link[j][i]) continue;
+            if (link[j - 1][i]) continue;
+            if (link[j + 1][i]) continue;
+
+            link[j][i] = true;
+            loop(i, cnt + 1);
+            link[j][i] = false;
+        }
+    }
+
+}
 
 int main() {
     cin >> N >> M >> H;
 
+//    link.resize(H, vector<bool>(N - 1, false));
+    int a, b;
+    for (int i = 0; i < M; i++) {
+        cin >> a >> b;
+        link[b][a] = true;
+    }
 
+    for(int i = 0; i < 4; i++) {
+        loop(1, i);
+    }
+//    loop(1, 0);
+
+    if (result == 99999999) cout << -1;
+    else cout << result;
 }
