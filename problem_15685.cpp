48fbbca54a356b16c738c98014788fb4ea9be4d1
소태호 <sth9057@gmail.com>
2024-09-06 21:16:13 +0900

    problem_15685.cpp failed
    옛날에 봤던 풀이법을 복기하여 문제를 해결했기에 failed
    
    드래곤 커브가 그려지는 모양이 일정한 패턴을 가지고 있으므로 패턴을 파악하는 게 중요
    
    시뮬레이션 문제는 패턴을 잘 찾아내고 구현할 때 침착하고 꼼꼼하게 하자.

--- problem_15685.cpp
+++ problem_15685.cpp
@@ -1,3 +1,81 @@
-//
-// Created by kacelab on 2024-03-27.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, X, Y, D, G, result = 0;
+bool map[101][101]; // map[y][x]
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+void draw(int x, int y, int d, int g) {
+    map[y][x] = true;
+    int nx = x + dx[d];
+    int ny = y + dy[d];
+
+    map[ny][nx] = true;
+    vector<int> arr;
+    arr.push_back(d);
+    for (int i = 0; i < g; i++) {
+        int size = arr.size();
+        for (int j = size - 1; j >= 0; j--) {
+            arr.push_back((arr[j] + 1) % 4);
+        }
+//        cout << endl;
+//        for (int k: arr) {
+//            cout << k << " ";
+//        }
+//        cout << endl;
+    }
+
+    for (int j = 1; j < arr.size(); j++) {
+        map[ny + dy[arr[j]]][nx + dx[arr[j]]] = true;
+        nx = nx + dx[arr[j]];
+        ny = ny + dy[arr[j]];
+    }
+//    for(int i : arr) {
+//        cout << i << " ";
+//    }
+//    cout << endl;
+}
+
+void check() {
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (map[i][j] && map[i + 1][j + 1] && map[i][j + 1] && map[i + 1][j])
+                result++;
+        }
+    }
+}
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> X >> Y >> D >> G;
+        draw(X, Y, D, G);
+//        for (int i = 0; i < 11; i++) {
+//            for (int j = 0; j < 11; j++) {
+//                cout << map[i][j] << " ";
+//            }
+//            cout << endl;
+//        }
+//        cout << endl;
+    }
+    check();
+    cout << result << endl;
+
+//    for (int i = 0; i < 11; i++) {
+//        for (int j = 0; j < 11; j++) {
+//            cout << map[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+}
+
+//0. 0
+//1. 0 1
+//2. 0 1 2 1
+//3. 0 1 2 1 2 3 2 1
+//4. 0 1 2 1 2 3 2 1 / 2 3 0 3 2 3 2 1
